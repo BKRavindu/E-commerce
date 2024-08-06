f2540123557ad33340868265313a75a87387ff60
BKRavindu <bkravindu2000@gmail.com>
2024-08-07 00:41:25 +0530

    push to git hub

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Include all JS, JSX, TS, TSX files in the src directory
+    './public/index.html', // Include the main HTML file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
